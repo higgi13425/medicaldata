e3deeb775ceafb283e71cdb9707b5ff671f4fc64
Peter Higgins <higgi13425@yahoo.com>
2021-07-17 12:37:35 -0400

    added an example of labelling with across() function to read labelled data and create fused numeric_label values.

---
+++ R/test_labelling.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(labelled)
+
+gender <- labelled(c(1,2,2,1,2), c(male = 1, female = 2))
+race <- labelled(c(3,1,4,1, 2), c(white = 1, black = 2, asian =3, amerindian =4))
+
+test <- tibble::tibble(gender, race)
+
+test %>%
+  mutate(across(.cols = gender:race,
+          .fns = ~paste0(., "_", as_factor(., levels="label"))))
